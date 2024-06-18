db45abdb91e5bf42e9217376b1873d01fa3152d1
Infidelol <kilvegan@gmail.com>
2024-06-17 18:18:11 -0700

    into

--- Rika.js
+++ Rika.js
@@ -3,4 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
         const welcomeMessage = document.getElementById('welcome-message');
         welcomeMessage.classList.add('fade-out');
     }, 3000); // Adjust the timeout duration as needed
+
+    window.addEventListener("scroll", function() {
+        const dropdown = document.querySelector(".dropdown");
+        if (window.scrollY > 100) { // Adjust the scroll position threshold as needed
+            dropdown.style.zIndex = "-1";
+        } else {
+            dropdown.style.zIndex = "1";
+        }
+    });
 });
